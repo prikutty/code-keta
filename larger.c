fb7cb21dc21ac160080d6268b5556e9d5d9e5598
prikutty <priyadhandapani0209@gmail.com>
2016-12-22 16:17:42 +0530

    Rename large.c to larger.c

---
+++ larger.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,a=100,b=50,c=25;
+printf("Enter the integer");
+scanf("%d",&n);
+if(a<=b)
+{
+printf("a is greater");
+}
+else if(b<=c)
+{
+printf("b is greater);
+}
+else
+{
+printf("c is greater");
+}
+}
