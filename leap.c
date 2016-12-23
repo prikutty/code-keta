386e4b11d834e1cdcffcdb35c3777a71ff318fef
prikutty <priyadhandapani0209@gmail.com>
2016-12-23 14:28:20 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -6,10 +6,18 @@ int main()
     printf("Enter any year: ");
     scanf("%d",&year);
 
-    if(((year%4==0)&&(year%100!=0))||(year%400==0))
+    if(((year%4==0)&&(year%100==0))||(year%400==0))
+    {
          printf("%d is a leap year",year);
+    }
+    else if((n%4==0)&&(n%100!=0))
+    {
+         printf("%d is  a leap year",year);
+    }
     else
-         printf("%d is not a leap year",year);
+    {
+        printf("%d is not a leap year",year);
+    }
   
     return 0;
 }
